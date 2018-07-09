bf675352d46666f90d9e705aea5e910a1c139e5c
za-liyiheng <liyiheng@zhongan.io>
2018-07-09 20:22:15 +0800

    etse

--- flink-test/socket-window-wordcount/src/main/java/test/flink/SocketWindowWordCount.java
+++ flink-test/socket-window-wordcount/src/main/java/test/flink/SocketWindowWordCount.java
@@ -9,80 +9,70 @@ import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.windowing.time.Time;
 import org.apache.flink.util.Collector;
 
-import java.io.IOException;
-import java.io.InputStream;
-
 @Slf4j
 public class SocketWindowWordCount {
-	public static void main(String[] args) throws Exception {
-		// the port to connect to
-		final int port;
-		try {
-			final ParameterTool params = ParameterTool.fromArgs(args);
-			port = params.getInt("port");
-			log.info("socket port: {}", port);
-		} catch (Exception e) {
-			System.err.println("No port specified. Please run 'SocketWindowWordCount --port <port>'");
-			return;
-		}
-		
-		processPropertiesFile();
-		
-		// get the execution environment
-		final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
-		
-		// get input data by connecting to the socket
-		DataStream<String> text = env.socketTextStream("localhost", port, "\n");
-		
-		// parse the data, group it, window it, and aggregate the counts
-		DataStream<WordWithCount> windowCounts = text
-				.flatMap(new FlatMapFunction<String, WordWithCount>() {
-					@Override
-					public void flatMap(String value, Collector<WordWithCount> out) {
-						for (String word : value.split("\\s")) {
-							out.collect(new WordWithCount(word, 1L));
-						}
-					}
-				})
-				.keyBy("word")
-				.timeWindow(Time.seconds(5), Time.seconds(1))
-				.reduce(new ReduceFunction<WordWithCount>() {
-					@Override
-					public WordWithCount reduce(WordWithCount a, WordWithCount b) {
-						return new WordWithCount(a.word, a.count + b.count);
-					}
-				});
-		
-		// print the results with a single thread, rather than in parallel
-		windowCounts.print().setParallelism(1);
-		
-		env.execute("Socket Window WordCount");
-	}
-	
-	private static void processPropertiesFile() throws IOException {
-		InputStream in = SocketWindowWordCount.class.getClassLoader().getResourceAsStream("test.properties");
-		ParameterTool params = ParameterTool.fromPropertiesFile(in);
-		log.info("params count: {}", params.getNumberOfParameters());
-		params.toMap().entrySet().forEach(e -> log.info("\t{} = {}", e.getKey(), e.getValue()));
-	}
-	
-	// Data type for words with count
-	public static class WordWithCount {
-		
-		public String word;
-		public long count;
-		
-		public WordWithCount() {
-		}
-		
-		public WordWithCount(String word, long count) {
-			this.word = word;
-			this.count = count;
-		}
-		
-		@Override
-		public String toString() {
-			return word + " : " + count;
-		}
-	}
+    public static void main(String[] args) throws Exception {
+        // the port to connect to
+        final int port;
+        final String host;
+        try {
+            final ParameterTool params = ParameterTool.fromArgs(args);
+            port = params.getInt("port");
+            host = params.get("host", "localhost");
+            log.info("socket host: {} port: {}", host, port);
+        } catch (Exception e) {
+            System.err.println("No port specified. Please run 'SocketWindowWordCount --port <port>'");
+            return;
+        }
+        
+        // get the execution environment
+        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        
+        // get input data by connecting to the socket
+        DataStream<String> text = env.socketTextStream(host, port, "\n");
+        
+        // parse the data, group it, window it, and aggregate the counts
+        DataStream<WordWithCount> windowCounts = text
+                .flatMap(new FlatMapFunction<String, WordWithCount>() {
+                    @Override
+                    public void flatMap(String value, Collector<WordWithCount> out) {
+                        for (String word : value.split("\\s")) {
+                            out.collect(new WordWithCount(word, 1L));
+                        }
+                    }
+                })
+                .keyBy("word")
+                .timeWindow(Time.seconds(5), Time.seconds(1))
+                .reduce(new ReduceFunction<WordWithCount>() {
+                    @Override
+                    public WordWithCount reduce(WordWithCount a, WordWithCount b) {
+                        return new WordWithCount(a.word, a.count + b.count);
+                    }
+                });
+        
+        // print the results with a single thread, rather than in parallel
+        windowCounts.print().setParallelism(1);
+        
+        env.execute("Socket Window WordCount");
+    }
+    
+    // Data type for words with count
+    public static class WordWithCount {
+        
+        public String word;
+        public long count;
+        
+        public WordWithCount() {
+        }
+        
+        public WordWithCount(String word, long count) {
+            this.word = word;
+            this.count = count;
+        }
+        
+        @Override
+        public String toString() {
+            return word + " : " + count;
+        }
+    }
 }
